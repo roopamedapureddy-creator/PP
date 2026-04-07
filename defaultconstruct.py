f953e56633cfa0b2be94bec79eae850e718d9e90
roopa@123 <roopamedapureddy@gmail.com>
2026-04-07 15:52:40 +0530

    Add files via upload

---
+++ defaultconstruct.py
@@ -0,0 +1,9 @@
+class constructadd:
+    def __init__(self):
+
+        self.x=100
+        self.y=100
+    def display(self):
+        print("Addition is: ",self.x+self.y)
+ca=constructadd()
+ca.display()
