96e505feec38c89bb1be13ef1799b1ac3b8f234f
roopa@123 <roopamedapureddy@gmail.com>
2026-04-07 15:51:26 +0530

    Add files via upload

---
+++ attr2.py
@@ -0,0 +1,7 @@
+class student:
+     college="AU"
+     def display(self,name):
+         print("name is:",name)
+         print("college name is: ",student.college)
+s1=student()
+s1.display("Hari")
