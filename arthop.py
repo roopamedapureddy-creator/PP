66acb368e4cba915bf7709bf58c03a5d260130c4
roopa@123 <roopamedapureddy@gmail.com>
2026-04-07 15:49:54 +0530

    Add files via upload

---
+++ arthop.py
@@ -0,0 +1,33 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=int(input("enter a number:"))
+enter a number:10
+>>> b=int(input("enter b number:"))
+enter b number:10
+>>> z=a+b
+>>> print(z)
+20
+>>> a=5
+>>> b=3
+>>> print("sum is: ",a+b)
+sum is:  8
+>>> print("sub is: ",a-b)
+sub is:  2
+>>> print(mul is: ",a*b)
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("mul is: ",a*b)
+...       
+mul is:  15
+>>> print("modulas is: ",a%b)
+...       
+modulas is:  2
+>>> print("division is: ",a/b)
+...       
+division is:  1.6666666666666667
+>>> print("floor div is: ",a//b)
+...       
+floor div is:  1
+>>> print("exponent is: ",a**b)
+...       
+exponent is:  125
