271f15270223ed0b8f3c3378a6133d34e6315ced
roopa@123 <roopamedapureddy@gmail.com>
2026-04-07 15:57:30 +0530

    Add files via upload

---
+++ listfun.py
@@ -0,0 +1,16 @@
+list1=[1,4,5,6,7]
+list2=[9,1,3,6,8]
+print(len(list2))
+print(min(list2))
+print(max(list2))
+a="bad boys"
+print(a)
+print(list(a))
+print(a.replace("bad","good"))
+print(sum(list1))
+print(sum(list1+list2))
+
+
+lists=[1,2,3,[4,5],8]
+print(lists)
+print(len(lists))
