a9505e25d4881b3edb542abbea69ea476d5edc50
roopa@123 <roopamedapureddy@gmail.com>
2026-04-07 15:57:12 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+list=["pink","black","puple","blue"]
+print(list)
+list=[1,2,3,4]
+list1=[5,6,7,8]
+print(list+list1)
+print(list*2)
+print(list[0:2])
+print(1 in list)
+print(5 not in list1)
+print(list[2])
+print(len(list1))
